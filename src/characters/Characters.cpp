acb0c664386ed05fba9773e8219b263affc3a4cb
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2022-07-13 17:34:09 -0400

    rotate-letter-on-their-axis

--- src/characters/Characters.cpp
+++ src/characters/Characters.cpp
@@ -31,16 +31,19 @@ void Characters::DrawF(TranslateMatrix *translateMatrix, float x_position, float
     Cube *cube = new Cube(255.0f, 255.0f, 255.0f, RenderMode::triangles);
     translateMatrix->setPosition(x_position, baseHeight + this->letterHeight, z_position);
     translateMatrix->setSize(this->lineWidth, this->letterHeight, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220, vec3(2, 1, 0));
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
     translateMatrix->setPosition(x_position+letterWidth-lineWidth, baseHeight + this->letterHeight+1, z_position);
     translateMatrix->setSize(letterWidth, this->lineWidth, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220);
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
     translateMatrix->setPosition(x_position+letterWidth-lineWidth, baseHeight + this->letterHeight + 3.5f, z_position);
     translateMatrix->setSize(letterWidth, this->lineWidth, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220);
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
@@ -51,30 +54,35 @@ void Characters::DrawR(TranslateMatrix *translateMatrix, float x_position, float
     Cube *cube = new Cube(255.0f, 255.0f, 255.0f, RenderMode::triangles);
     translateMatrix->setPosition(x_position, baseHeight + this->letterHeight, z_position);
     translateMatrix->setSize(this->lineWidth, this->letterHeight, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220, vec3(2, 0, 0));
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
     //right top column
     translateMatrix->setPosition(x_position + letterWidth + lineWidth*2, baseHeight + this->letterHeight+ 2.75f, z_position);
     translateMatrix->setSize(this->lineWidth, this->letterHeight/2-0.75f, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220, vec3(-1, 0, 0));
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
     //right bottom column
     translateMatrix->setPosition(x_position + letterWidth + lineWidth*2, baseHeight + this->letterHeight/2+0.25, z_position);
     translateMatrix->setSize(this->lineWidth, this->letterHeight/2 + 0.25, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220,vec3(-1, 0, 0));
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
     //middle bar
     translateMatrix->setPosition(x_position+letterWidth-lineWidth, baseHeight + this->letterHeight+1, z_position);
     translateMatrix->setSize(letterWidth/2, this->lineWidth, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220, vec3(0.5, 1, 0));
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
     //top bar
     translateMatrix->setPosition(x_position+letterWidth-lineWidth, baseHeight + this->letterHeight + 3.5f, z_position);
     translateMatrix->setSize(letterWidth -this->lineWidth, this->lineWidth, this->lineWidth);
+    translateMatrix->setObjectRotationAngle(220);
     translateMatrix->bindTranslationMatrix(this->shaderProgram);
     cube->Draw();
 
